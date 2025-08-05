767d4dbd34a88bcf2e7819a392b497a90cfc24b4
Jorge052011 <jimunozacuna+1@gmail.com>
2025-08-05 06:18:57 +0000

    W

--- users/urls.py
+++ users/urls.py
@@ -3,11 +3,11 @@
 
 from django.urls import path
 from .views import Login, Registro, Logout
+
 urlpatterns = [
     path("login/", Login.as_view(), name="login"),
-    path("register/", Registro.as_view() , name = "registro"),
-    path("logout/", Logout.as_view() , name = "logout"),
-
+    path("register/", Registro.as_view() , name = "register"),
+    path("logout/", Logout.as_view() , name = "logout")
 ]
 
 
